bbfd9a178f03845689d995bb34e19dfdf75f4117
sandayazisp <sandayazisp12@gmail.com>
2021-12-01 00:50:32 +0800

    membuat webpack devlopment dan mengubah script pada plugin copywebpackplugin()

--- webpack.common.js
+++ webpack.common.js
@@ -40,9 +40,6 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
-          globOptions: {
-            ignore: ['**/images/**'],
-          },
         },
       ],
     }),
