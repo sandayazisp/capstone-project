b2b5dad2b68a340e13acda0003d0c299d1d63fe1
sandayazisp <sandayazisp12@gmail.com>
2021-12-01 13:34:15 +0800

    membuat file content-footer.js

---
+++ src/scripts/component/content-footer.js
@@ -0,0 +1,18 @@
+class ContentFooter extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+    <footer class="py-3 my-4 bg-dark">
+    <div class="container">        
+        <p class="text-center text-muted w-75 m-auto border-bottom pb-3 mb-3 fst-italic">Our tour will give a larger opportunity to learn the culture, arts, religion, traditional life and the fantastic natural beauty of the surrounding country-side of Bali and the nearby islands.</p>        
+        <p class="text-center text-white fw-lighter">Copyright © 2021 - Mai.Tour</p>
+    </div>     
+</footer>   
+      `;
+  }
+}
+
+customElements.define('content-footer', ContentFooter);
