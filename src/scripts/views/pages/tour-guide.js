7299f7d9144944af4ca7098f4d8d399d52720a25
santicahyani <santicahyani51@gmail.com>
2021-12-16 16:16:28 +0800

    menambahkan file all tour guide & melengkapi pendaftaran

--- src/scripts/views/pages/tour-guide.js
+++ src/scripts/views/pages/tour-guide.js
@@ -56,13 +56,84 @@ const TourGuide = {
             </div>
         </div>
         
-        <section id="daftar">
-            <h2 class="text-center">Pendaftaran Guide Tour</h2> 
-            <p class="text-center">Dibuka pendaftaran pemandu wisata untuk menjual layanan anda secara online. <B>Ayo Daftar sebagai guide tour bali</p>
-            <div class="d-grid gap-1 col-2 mx-auto">
-                <button class="btn btn-primary center-block" type="button">Daftar Sekarang</button>
+        <div class="container px-2">
+        <h2 class="text-center pt-2 pb-4">All Tour Guide</h2>
+        <div class="row gx-3 mb-5 text-center">
+            <div class="col-4">
+                <div class="p-2 border bg-light">
+                <img src="images/guide/tour.png" class="img-thumbnail w-100 h-100" alt="tour guide">
+                <p class="fw-bold text-decoration-underline">Putu Bagia</p>
+                <p>Sebagai Guide Kota</p>
+                <p>Lokasi: Bali, Kabupaten Gianyar</p>
+                <p>Durasi = 18 jam/hari</p>
+                <p>Bahasa yang dikuasi:<br>
+                -Indonesia<br>
+                -Inggris</p>
+                <p>Pengalaman: pengalaman yang pernah saya dapatkan yaitu saya pernah memandu wisatawan dari eropa, autralia, inggris, jepang dan masih banyak lagi lainnya. saya telah menjadi pemandu wisata dari tahun 1987, banyaknya pengalaman yang saya dapatkan dari wisatawan yang banyak bicara, wisatwan yang royal, wisatawan yang dikit bicara dan lain sebagainya. Namun hal tersebut tidak membuat saya patah semangat karena saya sangat suka berwisata serta saya sangat suka berbagi ilmu serta pengalamn saya kepada orang lain. Ayo berlancong ke bali bali, kalau bukan kita yang memperkenalkan bali mau siapa lagi?. Sekian pengalaman saya yang dapat saya paparkan. Semoga lancar semua pendapatannya para tour guide. </p>
+                <p class="fw-bold">IDR 250.000/Hari || Max. 20 Orang </p>
+                </div>
             </div>
-        </section>
+            <div class="col-4">
+                <div class="p-2 border bg-light">
+                <img src="images/guide/tour.png" class="img-thumbnail w-100 h-100" alt="tour guide">
+                <p class="fw-bold text-decoration-underline">Nyoman Madia</p>
+                <p>Sebagai Guide Desa</p>
+                <p>Lokasi: Bali, Kabupaten Tabanan</p>
+                <p>Durasi = 18 jam/hari</p>
+                <p>Bahasa yang dikuasi:<br>
+                -Indonesia<br>
+                -Inggris<br>
+                -Jepang</p>
+                <p>Pengalaman: pengalaman yang saya miliki sangat beragam dari saya belum bisa menjadi tour guide terbaik sampai saya bisa menjadi tour guide terbaik seperti ini. Pengalaman yang saya alami mungkin tidak jauh berbeda dengan tour guide lainnya, seperti halnya melayani wisatawan yang bawel, pendiam, serta yang sangat ramah. Namun pengalaman saya yang sangat berkesan yaitu disaat saya menjadi tour guide ada seorang wistawan yang memberikan saya pembayaran lebih, yang membuat saya berpikir apakah semua wisatawan baik seperti ini. Namun setelah banyaknya saya melayani tidak semua wisatawan yang baik seperti itu.</p>
+                <p class="fw-bold">IDR 300.000/Hari || Max. 25 Orang </p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="p-2 border bg-light">
+                <img src="images/guide/tour.png" class="img-thumbnail w-100 h-100" alt="tour guide">
+                <p class="fw-bold text-decoration-underline">Komang Nadia Utari</p>
+                <p>Sebagai Guide Kota</p>
+                <p>Lokasi: Bali, Kabupaten Badung</p>
+                <p>Durasi = 18 jam/hari</p>
+                <p>Bahasa yang dikuasi:<br>
+                -Indonesia<br>
+                -Inggris</p>
+                <p>Pengalaman: pengalam yang saya miliki selama menjadi tour guide yaitu saya sangat senang job ini, saya bisa mengenal banyak wisatawan asing. Saya sangat santai dalam memandu wisatawan maka dari itu banyak nya wisatawan yang sangat menyukai saya sebagai tour guidenya. Sampainya terdapat wisatawan yang membelikan saya mobil yang dikarenakan saya dulu menggunakan mobil kecil untuk memandu wisatawan. Ini adalah pengalaman saya yang sangat berkesan dalam hidup saya. Rasa simpati dari seorang wisatawan sangat besar, padahal saya sendiri baru memandu dia hanya satu kali. Namun wisatawan tersebut telah mempercayai saya. Pesan saya jadilah orang baik, bertanggung jawab serta jujur, karena kpercayaan sulit dicari.</p>
+                <p class="fw-bold">IDR 250.000/Hari || Max. 20 Orang </p>
+                </div>
+            </div>
+      </div>
+      </div>
+
+      <section id="daftar">
+      <div class="row justify-content-evenly">
+          <div class="col-4">
+          <h5 class="text-start">Syarat Pendaftaran Tour Guide</h5>
+          <ul>
+              <li> Laki-laki/perempuan berusia minimal 18 Tahun</li>
+              <li> Memiliki pengalaman bekerja dibidang pariwisata</li>
+              <li> Bersedia menjaga kelestarian pariwisata indonesia dan nama baik Tour Guide</li>
+              <li> Memiliki smartphone dengan aspek yang dapat mengakses internet dengan baik</li>
+          </ul> 
+          <h5 class="text-start">Dokumen Yang Perlu Disiapkan</h5>
+          <ul>
+              <li> KTP</li>
+              <li> CV (Opsional)</li>
+              <li> Sertifikat profesi pemandu wisata</li>
+              <li> Rekening Bank</li>
+          </ul> 
+          <p class="fst-italic">*semua dokumen yang perlu disiapkan dikirimkan lewat email</p>
+          </div>
+          <div class="col-4">
+          <h2 class="text-center">Pendaftaran Tour Guide</h2> 
+          <p class="text-center">Dibuka pendaftaran pemandu wisata untuk menjual layanan anda. <B>Ayo Daftar sebagai guide tour bali</B></p>
+          <p class="fst-italic text-center">klik daftar sekarang untuk menuju ke email</p>
+          <div class="d-grid gap-1 col-4 mx-auto">
+              <button class="btn btn-primary center-block" type="button">Daftar Sekarang</button>
+          </div>
+          </div>
+      </div>
+      </section>
 
         <div id="info__tour">
         <div class="container p-5">               
