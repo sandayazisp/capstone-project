b042480a9bfec148d649917e3c2ab385588de2d6
sandayazisp <sandayazisp12@gmail.com>
2021-12-20 16:34:52 +0800

    menambahkan file sw.js dan cache_helper.js untuk membuat website bisa diakses seperti aplikasi native

---
+++ src/scripts/sw.js
@@ -0,0 +1,18 @@
+const { default: CacheHelper } = require('./utils/cache-helper');
+
+/* eslint-disable no-restricted-globals */
+const { assets } = global.serviceWorkerOption;
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+  // TODO: Caching App Shell Resource
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(CacheHelper.deleteOldCache());
+
+  // TODO: Delete old caches
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(CacheHelper.revalidateCache(event.request));
+});
