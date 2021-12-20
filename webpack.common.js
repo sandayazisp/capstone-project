db06b3d34ca9374e4f7276c9aeeeb8369c647980
sandayazisp <sandayazisp12@gmail.com>
2021-12-20 16:39:22 +0800

    membuat file optimasi resolusi gambar

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminWebp = require('imagemin-webp');
+const imageminSvgo = require('imagemin-svgo');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
@@ -40,7 +47,33 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/images/**'],
+          },
         },
+      ],
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        imageminPngquant({
+          quality: [0.3, 0.5],
+        }),
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
+        imageminWebp({
+          quality: 50,
+        }),
+        imageminSvgo({
+          plugins: [{
+            name: 'removeViewBox',
+            active: false,
+          }],
+        }),
       ],
     }),
   ],
