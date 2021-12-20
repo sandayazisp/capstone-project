8cd6ac3e5efb86d2a72ce88bfc32f44c1a0cc96d
sandayazisp <sandayazisp12@gmail.com>
2021-12-20 16:42:17 +0800

    memperbaiki content pada tag details dan juga memperbaiki content hero

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -17,22 +17,46 @@ const Home = {
         
                 <div
                     class="container position-absolute top-50 start-50 translate-middle bg-dark bg-opacity-50 p-3 text-white w-100 d-flex align-items-center justify-content-center flex-column">
-                    <h1>Welcome To</h1>
+                    <h1>Selamat Datang Di</h1>
                     <h1 class="text-md-start">BALI</h1>
                     <p class="text-center">Bali merupakan destinasi tempat wisata yang terkenal di seluruh manca negara. Keindahan pantai, gunung serta persawahan yang dimiliki oleh bali membuat ketertarikan wisatawan mengunjungi bali</p>                    
-                    <a class="btn btn-outline-warning" href="#/requirement" role="button">Requirements </a>
+                    <a class="btn btn-outline-warning" href="#/requirement" role="button">Peraturan </a>
                 </div>
         
                 <!-- The slideshow/carousel -->
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="images/hero/hero-1.jpg" alt="Melasti Beach" class="d-block">
+                        
+                        <picture>
+                          <source media="(max-width: 600px)" srcset="./images/hero-1-small.jpg">
+                          <source media="(max-width: 900px)" srcset="./images/hero-1-large.jpg">
+                          <img 
+                              src="./images/hero-1-xl.jpg" 
+                              alt="Melasti Beach" class="d-block"></img>
+                      
+                      </picture>
                     </div>
                     <div class="carousel-item">
-                        <img src="images/hero/hero-2.jpg" alt="GWK Bali" class="d-block">
+                        
+                        <picture>
+                          <source media="(max-width: 600px)" srcset="./images/hero-2-small.jpg">
+                          <source media="(max-width: 600px)" srcset="./images/hero-2-large.jpg">
+                          <img 
+                              src="./images/hero-2-xl.jpg" 
+                              alt="GWK Bali" class="d-block"></img>
+                      
+                      </picture>
                     </div>
                     <div class="carousel-item">
-                        <img src="images/hero/hero-3.jpg" alt="Nusa Penida" class="d-block">
+                        
+                        <picture>
+                          <source media="(max-width: 600px)" srcset="./images/hero-3-small.jpg">
+                          <source media="(max-width: 600px)" srcset="./images/hero-3-large.jpg">
+                          <img 
+                              src="./images/hero-3-xl.jpg" 
+                              alt="Nusa Penida" class="d-block"></img>
+                      
+                      </picture>
                     </div>
                 </div>
         
@@ -48,10 +72,15 @@ const Home = {
 
         <div id="mainContent">
             <div class="container pt-5" id="about__bali">
-                <h1 class="text-center pt-2 pb-5">About Bali</h1>
+                <h1 class="text-center pt-2 pb-5">Tentang Bali</h1>
                 <div class="row row-cols-1 row-cols-md-2">
-                    <div class="col">                        
-                        <img src="images/gwk 2.png" class="img-thumbnail w-100 h-75" alt="Patung GWK">
+                    <div class="col">                                              
+                        <picture>
+                          <source media="(max-width: 600px)" srcset="./images/gwk-small.jpg">
+                          <source media="(max-width: 1024px)" srcset="./images/gwk-large.jpg">
+                          <img src="./images/gwk-xl.jpg" class="img-thumbnail w-100 h-75" alt="Patung GWK">
+                      
+                      </picture>
                     </div>
                     <div class="col">
                         <p class="ps-3 pe-3">Bali juga dikenal dengan sebutan Pulau Dewata, Pulau Seribu Pura, atau Pulau Surga. Karena di samping pantai-pantai Bali yang memang sudah sangat terkenal keindahannya, Bali juga memiliki keindahan alam yang luar biasa dan lengkap, seperti gunung berapi, sawahnya yang bersusun (Terasering Ceking) menghampar hijau memberikan rasa damai dan ketenangan, butiran pasir dan keindahan alam bawah lautnya yang mengagumkan seolah menjadi persembahan keindahan alam yang tiada habisnya, serta keunikan seni budayanya dan berbagai hasil kerajinan tangan yang khas dan fantastis dari para pengrajinnya. Sesuai dengan apa yang disampaikan aktor kenamaan Hollywood, Richard Gere, bahwa masyarakat Bali dianggap tinggal di surga (paradise) yang sangat menyenangkan. Bali adalah tempat berkumpulnya berbagai wisatawan dari seluruh dunia. Bagi wisatawan asing pantai-pantai yang ada di Bali memiliki ombak-ombak yang terkenal sebagai lokasi surfing untuk berselancar seperti di pantai Kuta, Uluwatu, dan pantai Dreamland. Bagi mereka yang suka dengan hal-hal yang menantang yang memacu adrenalin seperti menyelam, rafting, treking dan lainnya semua juga bisa dilakukan di Bali. Ada banyak lokasi wisata yang sayang apabila dilewatkan ketika Anda berada di Bali. Mulai dari wisata pantai, pegunungan, danau, hutan, kebun binatang, atau desa beserta masyarakat Bali yang pada umumnya masih kuat memegang teguh tradisi peninggalaan nenek moyangnya.</p>
@@ -61,7 +90,7 @@ const Home = {
 
             <div id="rek__wisata" class="pt-5 pb-5">
                 <div class="container">
-                    <h1 class="text-center pb-5 text-white">Recomended Objek Wisata</h1>
+                    <h1 class="text-center pb-5 text-white">Rekomendasi Objek Wisata</h1>
                     <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 rek__wisata">
                         <!-- Item Rekomendasi wisata dari api -->
                     </div>
@@ -74,15 +103,14 @@ const Home = {
             <div id="rek__guidetour">
                 <div class="parallax">
                     <div class="container p-5">
-                        <h1 class="text-center pb-5 text-white">Tour Guide</h1>
+                        <h1 class="text-center pb-5 text-white">Pemandu Wisata</h1>
                         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 pb-5 border-bottom guideToure">
                                                      
                         </div>
-                        <h2 class="text-center text-white pt-3 pb-3">Quick Response ?</h2>
-                        <h2 class="text-center text-white pb-3">Please Contact Us</h2>                                                
-                        <div class="d-grid d-flex gap-2 col-12 justify-content-center mx-auto">
-                            <a class="btn btn-outline-info" href="#" role="button">Call Us</a> 
-                            <a class="btn btn-outline-info" href="#" role="button">Whatsapp</a>
+                        <h2 class="text-center text-white pt-3 pb-3">Hubungi Admin</h2>
+                        <h3 class="text-center text-white pb-3">Untuk Mendapatkan Pemandu Wisata yang Baik dan Profesional</h3>                                                                                                                  
+                        <div class="d-grid d-flex gap-2 col-12 justify-content-center mx-auto">                         
+                            <a class="btn btn-outline-info" href="http://wa.me/+6287865950912" target="_blank" rel="noopener" role="button">Whatsapp</a>
                           </div>                       
                     </div>
                 </div>                            
@@ -97,7 +125,7 @@ const Home = {
                           <div class="card text-center border-0 shadow-none">
                             <div class="card-body">
                               <h5 class="card-title">Memiliki Jaringan Luas</h5>
-                              <img src="images/mind_map.svg" class="card-img w-50 h-50 pt-3 pb-3" alt="...">
+                              <img src="mind_map.svg" class="card-img w-50 h-50 pt-3 pb-3" alt="Jaringan Luas">
                               <p class="card-text">Memiliki jaringan yang luas membuat kami mampu menyediakan berbagai macam tur dan perjalanan wisata yang kamu inginkan.</p>                              
                             </div>
                           </div>
@@ -106,7 +134,7 @@ const Home = {
                           <div class="card text-center border-0 shadow-none">
                             <div class="card-body">
                               <h5 class="card-title">Profesional</h5>
-                              <img src="images/handshake.svg" class="card-img w-50 h-50 pt-3 pb-3" alt="...">
+                              <img src="handshake.svg" class="card-img w-50 h-50 pt-3 pb-3" alt="Profesional">
                               <p class="card-text">Mai.Tour memiliki team profesional yang akan membantu merancang rencana perjalanan sesuai yang kamu minati.</p>                              
                             </div>
                           </div>
@@ -115,7 +143,7 @@ const Home = {
                           <div class="card text-center border-0 shadow-none">
                             <div class="card-body">
                               <h5 class="card-title">Aktivitas & Kreasi Pilihan</h5>
-                              <img src="images/money_transfer.svg" class="card-img w-50 h-50 pt-3 pb-3" alt="...">
+                              <img src="money_transfer.svg" class="card-img w-50 h-50 pt-3 pb-3" alt="Ativitas & Kreasi">
                               <p class="card-text">Kepuasan anda adalah proritas bagi kami. Seluruh aktivitas tur dan rekreasi memiliki kualitas terbaik di bali.</p>                              
                             </div>
                           </div>
